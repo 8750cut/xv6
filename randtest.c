5cb7f53e657c3d020966847c4f2399525ff60aba
Cristiano Miranda <cristiano.jesus@stonybrook.edu>
2016-04-12 15:08:52 -0400

    Add tests to randtest

--- randtest.c
+++ randtest.c
@@ -10,7 +10,7 @@ test()
   int pass = 1;
 
   printf(1, "TESTING RANDOM WITH RANGE\n");
-  for(i = 0, j = 400000; i < 200000; i+=100, j-=100)
+  for(i = 0, j = 4000; i < 2000; i+=100, j-=100)
   {
     printf(1, "FROM %d TO %d: ", i, j);
 
@@ -27,6 +27,34 @@ test()
       printf(1, "OK\n");
     k = 0;
     pass = 1;
+  }
+
+  int indexes[100];
+
+  printf(1, "TESTING DISTRIBUTION FROM 0 TO 50\n");
+  for(i = 0; i < 100; i++)
+  {
+    indexes[i] = 0;
+  }
+
+  int min = 0, max = 100;
+  for(i = 0; i < 10000; i++)
+  {
+    x = random() % 50;
+    if(x < min || x > max)
+    {
+      printf(1, "INVALID OUTPUT: %d\n", x);
+      break;
+    }
+    indexes[x]++;
+  }
+
+
+  int index = 0;
+  while(index < 50)
+  {
+    printf(1, "indexes[%d] = %d\n", index, indexes[index]);
+    index++;
   }
 }
 
