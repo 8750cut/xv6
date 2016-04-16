c1c7fe78d262466f8f341cd94862ee490b8cd850
Jessica Zeng <jeszeng@cs.stonybrook.edu>
2016-04-15 23:51:15 -0400

    updated lotterytest

--- lotterytest.c
+++ lotterytest.c
@@ -7,47 +7,34 @@ forktest(){
 	printf(1, "starting fork test\n");
 	// int i, j, k, n, pid, sum;
 
-	int a[3][3], b[3][3], c[3][3];
-	int i, j, k, n, pid, sum;
+	int a[5][5], b[5][5], c[5][5];
+	int i, j, k, n, z, pid, sum;
 
-	for(i=1; i<=3; i++){
-		for(j=i-1; j<=3; j++){
+	for(i=1; i<=5; i++){
+		for(j=i-1; j<=5; j++){
 			a[i][j] = j;
 			b[i][j] = j;
 		}
 	}
 
-	// for(n=0; n<3; n++){
-	//     pid = fork();
-	//     if(pid < 0)
-	//       break;
-	//     if(pid == 0){
-	//     	nice(n);
-	//     	printf(1, "forked child %d with priorty %d\n", n, n);
-	//     	for (i = 0; i < 3; i++) {
-	// 		    for (j = 0; j < 3; j++) {
-	// 		        sum = 0;
-	// 		        for (k = 0; k < 3; k++) {
-	// 		            sum = sum + a[i][k] * b[k][j];
-	// 		        }
-	// 		        c[i][j] = sum;
-	// 		    }
-	// 		    printf(1, "child %d row %d completed with values: %d %d %d\n", n, i, c[i][0], c[i][1], c[i][2]);
-	// 	    }
-	// 	    exit();
-	//     }
-	//   }
-	for(n=0; n<3; n++){
+	for(n=0; n<5; n++){
 	    pid = fork();
 	    if(pid < 0)
 	      	break;
 	    if(pid == 0){
-	    	nice(n);
-	    	printf(1, "forked child %d with priorty %d\n", n, n);
-	    	for (i = 0; i < 3; i++) {
-			    for (j = 0; j < 3; j++) {
+	    	if(n == 1)
+	    		z = -19;
+	    	else if(n == 2)
+	    		z = 12;
+	    	else
+	    		z = n;
+
+	    	nice(z);
+	    	printf(1, "forked child %d with priorty %d\n", n, z);
+	    	for (i = 0; i < 5; i++) {
+			    for (j = 0; j < 5; j++) {
 			        sum = 0;
-			        for (k = 0; k < 3; k++) {
+			        for (k = 0; k < 5; k++) {
 			            sum = sum + a[i][k] * b[k][j];
 			        }
 			        c[i][j] = sum;
